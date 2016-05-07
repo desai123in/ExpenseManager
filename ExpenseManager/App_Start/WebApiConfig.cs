f10d47a0806aef0eb8cd460d18124b0c25410c8c
desai123in <ddesai.us@gmail.com>
2016-05-07 14:52:27 -0400

    d

--- ExpenseManager/App_Start/WebApiConfig.cs
+++ ExpenseManager/App_Start/WebApiConfig.cs
@@ -1,24 +1,29 @@
-﻿using System;
+﻿using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 
-namespace ExpenseManager
+namespace ExpenseManager.App_Start
 {
-    public static class WebApiConfig
+    public class WebApiConfig
     {
-        public static void Register(HttpConfiguration config)
+        public static void Configure(IAppBuilder app)
         {
-            // Web API configuration and services
+            HttpConfiguration config = new HttpConfiguration();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(                name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional });
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            // Json formatter
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            app.UseWebApi(config);
         }
     }
 }
---
+++ ExpenseManager/Startup.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseManager.App_Start;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseManager
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            WebApiConfig.Configure(app);
+        }
+    }
+}
